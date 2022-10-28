271f48d6c1470fc7ca5267584a6f97645bb102f1
abhikausal22 <44508144+abhikausal22@users.noreply.github.com>
2022-10-28 20:29:19 +0530

    Create ScientificCalci.py

---
+++ python/ScientificCalci.py
@@ -0,0 +1,26 @@
+import math
+ 
+# create function declarations
+def add(a, b):
+    return a + b
+ 
+def subtract(a, b):
+    return a - b
+ 
+def divide(a, b):
+    return a/b
+ 
+def multiply(a, b):
+    return a*b
+ 
+def square(a):
+    return a**2
+ 
+def sqrt(a):
+    return math.sqrt(a)
+ 
+def log(a):
+    return math.log(a)
+ 
+def exponentiate(a, b):
+    return a**b
